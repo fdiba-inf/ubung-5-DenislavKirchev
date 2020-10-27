72e40eccede38fc1aadadff00a80729eef887f1a
DenislavKirchev <denislav_delon@abv.bg>
2020-10-27 18:36:43 +0000

    .

--- src/exercise5/UniqueNumbers.java
+++ src/exercise5/UniqueNumbers.java
@@ -18,7 +18,16 @@ public class UniqueNumbers {
         }
 
         int[] uniqueNumbers = new int[numbers.length];
-        // Find unique numbers in numbers
+        
+        uniqueNumbers[0] = numbers[0];
+        int uniqueIndex=1;
+        for (int index = 1; index < numbers.length; index++) {
+          
+          if(numbers[index]!=numbers[index-1]){
+            uniqueNumbers[uniqueIndex]=numbers[index];
+            uniqueIndex++;
+          }  
+        }
 
         String uniqueNumbersAsString = Arrays.toString(uniqueNumbers);
         System.out.println("Unique numbers: " + uniqueNumbersAsString);
